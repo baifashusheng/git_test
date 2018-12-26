1d6eb46cc551d41bcf2adb95ed5dc5786e33ffc1
AUthor Name <author email>
2018-12-26 23:40:45 +0800

    test : add init code for test project
    project
    
    Signed-off-by: AUthor Name <AUthor Email>

---
+++ src/kernel/poller.cpp
@@ -0,0 +1,77 @@
+//
+// Created by 陈家阔 on 2025/8/18.
+//
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#ifdef __linux__
+#include <sys/epoll.h>
+#include <sys/timerfd.h>
+#else
+#include <sys/event.h>
+# undef LIST_HEAD
+# undef SLIST_HEAD
+#endif
+#include <errno.h>
+#include <limits.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <openssl/ssl.h>
+#include "list.h"
+#include "rbtree.h"
+#include "poller.h"
+
+#define POLLER_BUFSIZE (256 * 1024)
+#define POLLER_EVENTS_MAX 256
+
+struct __poller_node{
+    int state;
+    int error;
+    struct poller_data data;
+#pragma pack(1)
+    union{
+        struct list_head list;
+        struct rb_node rb;
+    };
+#pragma pack()
+    char in_rbtree;
+    char rbmoved;
+    int event;
+    struct timespec timeout;
+    struct __poller_node *res;
+};
+
+struct __poller{
+    size_t max_open_files;
+    void (*callback)(struct poller_result *, void *);
+    void *context;
+
+    pthread_t tid;
+    int pfd;
+    int timerfd;
+    int pipe_rd;
+    int pipe_wr;
+    int stopped;
+    struct rb_root timeo_tree;
+    struct rb_node *tree_first;
+    struct rb_node *tree_last;
+    struct list_head timeo_list;
+    struct list_head no_timeo_list;
+    struct __poller_node **nodes;
+    char buf[POLLER_BUFSIZE];
+};
+
+#ifdef __linux__
+
+static inline int __poller_create_pfd()
+{
+    return epoll_create(1);
+}
+
+static inline int __poller_close_pfd(int fd)
+{
+    return close(fd);
+}
---
+++ src/kernel/poller.h
@@ -0,0 +1,101 @@
+//
+// Created by 陈家阔 on 2025/6/10.
+//
+
+#ifndef _POLLER_H_
+#define _POLLER_H_
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+typedef struct __poller poller_t;
+typedef struct __poller_message poller_message_t;
+
+struct __poller_message{
+    int (*append)(const void *, size_t *, poller_message_t *);
+    char data[0];
+};
+
+struct poller_data{
+#define PD_OP_TIMER         0
+#define PD_OP_READ          1
+#define PD_OP_WRITE         2
+#define PD_OP_LISTEN        3
+#define PD_OP_CONNECT       4
+#defien PD_OP_RECVFROM      5
+#defien PD_OP_SSL_READ      PD_OP_READ
+#defien PD_OP_SSL_WRITE     PD_OP_WRITE
+#define PD_OP_SSL_ACCEPT    6
+#define PD_OP_SSL_CONNECT   7
+#define PD_OP_SSL_SHUTDOWN  8
+#define PD_OP_EVENT         9
+#define PD_OP_NOTIFY        10
+    short operation;
+    unsigned short iovcnt;
+    int fd;
+    SSL *ssl;
+    union{
+      poller_message_t *(*create_message)(void *);
+      int (*partial_written)(size_t,void *);
+      void *(*accept)(const struct sockaddr *, socklen_t, int, void *);
+      void *(recvfrom)(const struct sockaddr *, socklen_t, const void *, size_t, void *);
+      void *(*event)(void *);
+      void *(notify)(void *, void *);
+    };
+    void *context;
+    union{
+        poller_message_t *message;
+        struct iovec *write_iov;
+        void *result;
+    };
+};
+
+struct poller_result
+{
+#define PR_ST_SUCCESS  0
+#define PR_ST_FINISHED 1
+#define PR_ST_ERROR    2
+#define PR_ST_DELETED  3
+#define PR_ST_MODIFIED 4
+#define PR_ST_STOPPED  5
+    int state;
+    int error;
+    sturct poller_data data;
+};
+
+struct poller_params
+{
+    size_t max_open_files;
+    void (*callback)(struct poller_result *, void *);
+    void *context;
+};
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    poller_t *poller_create(const struct poller_params *params);
+    int poller_start(poller_t *poller);
+    int poller_add(const struct pollar_data *data, int timeout, poller_t *poller);
+    int poller_del(int fd, poller_t *poller);
+    int poller_mod(const struct poller_data *data, int timeout, poller_t *poller);
+
+};
+
+poller_t *poller_create(const struct poller_params *params);
+int poller_start(poller_t *poller);
+int poller_add(const struct poller_data *data, int timeout, poller_t *poller);
+int poller_del(int fd, poller_t *poller);
+int poller_mod(const struct poller_data *data, int timeout, poller_t *poller);
+int poller_set_timeout(int fd, int timeout, poller_t *poller);
+int poller_add_timer(const struct timespace *value, void *context, void **timer, poller_t *poller);
+int poller_del_time(void *timer, poller_t *poller);
+void poller_stop(void *timer, poller_t *poller);
+void poller_destroy(poller_t *poller);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_POLLER_H_
